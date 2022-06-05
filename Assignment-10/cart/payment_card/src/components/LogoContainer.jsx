694378404ec1fb4e934bb1c9af65906ba3e66513
mayuri3598 <97494267+mayuri3598@users.noreply.github.com>
2022-06-05 19:46:56 +0530

    done

---
+++ Assignment-10/cart/payment_card/src/components/LogoContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Styles from "./LogoConatiner.module.css";
+export const LogoContainer = ({date,logo,color}) => {
+  return (
+    <div className={`${Styles.logo_container} ${color=="golden"?Styles.golden:Styles.grey}`}>
+        <h5 className={Styles.h5}>{date}</h5>
+       <img src={logo} alt="" />
+    </div>
+  )
+}
